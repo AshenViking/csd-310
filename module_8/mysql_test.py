5ba4e9f3eb4d70fd9e5aecbf4a6a92e24fe03b3c
AshenViking <93166218+AshenViking@users.noreply.github.com>
2022-02-04 17:44:05 -0600

    Add files via upload

--- module_8/mysql_test.py
+++ module_8/mysql_test.py
@@ -1,7 +1,7 @@
 import mysql.connector
 from mysql.connector import errorcode
 config={
-    "user": "pysports_user",
+    "user": "pytest_user",
     "password": "MySQL8IsGreat!",
     "host": "127.0.0.1",
     "database": "pysports",
